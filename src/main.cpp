97a6f057b4abf5d87ab1ffe96b381ac29f09f4e7
DaniilBukov <bukovda@gmail.com>
2022-10-31 02:16:46 +0300

    added const char vertex_buffer_object

--- src/main.cpp
+++ src/main.cpp
@@ -64,6 +64,24 @@ int main(void)
 		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
 
+		/* create vertex shader */
+		unsigned int vertexShader;
+		vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+		/* compiling vertex shader from source file or string */
+		glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+		glCompileShader(vertexShader);
+
+		int success;
+		char infoLog[512];
+		glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+		if(!success)
+		{
+			glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+			std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+		}
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
