de5192085dfc3838c1008d931250d7b1e36301a8
DaniilBukov <bukovda@gmail.com>
2022-10-31 11:11:28 +0300

    2 triangles

--- src/main.cpp
+++ src/main.cpp
@@ -125,21 +125,32 @@ int main(void)
 
 		/* triangle vertex array */
 		float vertices[] = {
+				 0.5f,  0.5f, 0.0f,
+			 	 0.5f, -0.5f, 0.0f,
 				-0.5f, -0.5f, 0.0f,
-				0.5f, -0.5f, 0.0f,
-				0.0f, 0.5f, 0.0f
+				-0.5f,  0.5f, 0.0f
+		};
+		unsigned int indices[] = {
+			0, 1, 3,
+			1, 2, 3
 		};
 
-		/* create VBO and VAO */
-		unsigned int VBO, VAO;
+		/* create VBO, VAO */
+		unsigned int VBO, VAO, EBO;
 		glGenVertexArrays(1, &VAO);
 		glGenBuffers(1, &VBO);
+		glGenBuffers(1, &EBO);
 
-		/* copy array of vertices to buffer */
+		/*link VAO*/
 		glBindVertexArray(VAO);
 
+		/* copy array of vertices to vertex buffer */
 		glBindBuffer(GL_ARRAY_BUFFER, VBO);
 		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+		/*  */
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 		/* create pointers for vertex attributes */
 		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
@@ -151,7 +162,8 @@ int main(void)
 		/* unlink VAO */
 		glBindVertexArray(0);
 
-
+		/* line mode */
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
 
 
@@ -168,7 +180,8 @@ int main(void)
 				/* use shader program to visualise the object */
 				glUseProgram(shaderProgram);
 				glBindVertexArray(VAO);
-				glDrawArrays(GL_TRIANGLES, 0, 3);
+				glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+				glBindVertexArray(0);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
